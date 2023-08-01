70eb5d1e217c40a2d477fc432e15960f3076b241
mazharulislam-12 <505mazharulislam@gmail.com>
2023-08-01 11:11:42 -0700

    peramitter sum

--- peramiter.js
+++ peramiter.js
@@ -3,3 +3,16 @@ function bringSingara(money){
     console.log(money);
 }
 bringSingara(100);
+
+
+function add(num1, num2) {
+    console.log('going to add: ', num1, num2);
+}
+add(120)
+
+function sum(a, b, c, d, e) {
+    console.log(a, b, c, d, e);
+    var sum = a + b + c + d + e;
+    console.log(sum);
+}
+sum(45, 85,63, 45, 87);
