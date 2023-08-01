2e68251e758792b05052b46b9ac6fb86126f41d8
mazharulislam-12 <505mazharulislam@gmail.com>
2023-08-01 11:44:59 -0700

    return for js

---
+++ retun.js
@@ -0,0 +1,17 @@
+function add(number1, number2) {
+    console.log(number1, number2);
+    var sum = number1 + number2;
+    return sum;
+}
+var total = add(80, 20)
+console.log('Total', total);
+
+
+function bringSingara(money) {
+    var singraPrice = 10;
+    var quantity = money / singraPrice;
+    return quantity;
+}
+var myTaka = 150;
+var singaras = bringSingara(myTaka);
+console.log('Eating singra', singaras);
