4a9ddf572fa1dd31d76453acbdb374dfa9ddb75a
mazharulislam-12 <505mazharulislam@gmail.com>
2023-08-02 08:19:39 -0700

    function, retun, and object

---
+++ object.js
@@ -0,0 +1,24 @@
+var student = {
+    id: 151,
+    name : 'Alia Khan',
+    class : 10, 
+    marks : 98
+}
+console.log(student);
+var mobile = {
+    brand: 'Samsung',
+    price : 19000,
+    strog : '64gb',
+    camara : "7mp" 
+}
+console.log(mobile);
+
+var myComputer = {
+        brand: 'asus',
+        price : 49000,
+        color: 'silver',
+        processor: 'i3'
+}
+myComputer.processor = 'i5';
+console.log(myComputer);
+console.log(myComputer.brand);
